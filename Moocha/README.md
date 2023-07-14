ed33e8d5189f351f909f8d48724c09225ed1ef3e
Matheus Cardim Torres <matheus.torres@convergint.com>
2023-07-14 14:04:25 -0300

    Only Moocha

---
+++ Moocha/README.md
@@ -0,0 +1,2 @@
+# Moocha
+Moocha is a website of a bookstore focused on being comfortable and visually pleasing to the user
--- Moocha/src/App.jsx
+++ Moocha/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import moochaLogo from './assets/logo.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +9,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <header>
+        <a href="http://localhost:5173/" className='logoA'>
+          <img src={moochaLogo} className="logo" alt="Moocha logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <nav>
+          <a href='/#'>Home</a>
+          <a href='/#'>Products</a>
+          <a href='/#'>Social Medias</a>
+          <a href='/#'>About Us</a>
+        </nav>
+      </header>
+
     </>
   )
 }
